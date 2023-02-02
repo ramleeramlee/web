0f58a4d5601ab1d52ec73ef5aa9e8bc9872ede31
ramleeramlee <124111118+ramleeramlee@users.noreply.github.com>
2023-02-02 17:54:52 +0900

    day04

---
+++ day04/out.js
@@ -0,0 +1,19 @@
+   // 함수는 여러절차가 있는 처리를 이름을 부여하여 기능을 정의!
+   function 빨래하다() {
+    console.log("1. 먼지를 털자.")
+    console.log("2. 물에 집어 넣는다.")
+    console.log("3. 비누를 묻힌다.")
+    console.log("4. 오물조물 비빈다.")
+    console.log("5. 물에 여러번 헹군다.")
+    console.log("6. 짠다.")
+    console.log("7. 넌다.")
+  }
+  
+
+  //청소하다에 대한 함수정의해서 사용해보기
+  function 청소하다() {
+    console.log("1. 창문을 연다.")
+    console.log("2. 먼지를 턴다.")
+    console.log("3. 청소기를 돌린다.")
+    console.log("4. 물걸레질 한다.")
+  }
---
+++ day04/out2.js
@@ -0,0 +1,15 @@
+function 여행하다() {
+    console.log("1. 가고싶은 곳을 정한다.")
+    console.log("2. 날짜를 정한다.")
+    console.log("3. 계획을 짠다.")
+    console.log("4. 숙소를 예약한다.")
+   
+  }
+  
+
+  
+  function 쉬다() {
+    console.log("1. 침대위에 뒹굴뒹굴하기.")
+    console.log("2. 맛있는거 먹으면서 넷플보기.")
+   
+  }
